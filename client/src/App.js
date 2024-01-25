7485dcb68e5f77c6bacd47abe8727a08d7f4ae20
kosta-rizov <kosta.rizov85@gmail.com>
2024-01-25 01:44:34 +0100

    Redux Toolkit Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+
+  const theme = useMemo(() => {
+    return createTheme(themeSettings(mode));
+  }, [mode]);
+
   return (
     <div>
-      <h1>hello</h1>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <h1>hello</h1>
+      </ThemeProvider>
     </div>
   );
 }
